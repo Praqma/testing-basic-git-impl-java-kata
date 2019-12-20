f119fb794737d3240d50efc872a8b3b3340a633c
Samuel Ytterbrink <say@praqma.net>
2019-12-20 14:58:34 +0100

    !!! make static method none static

--- src/main/java/FileSystem.java
+++ src/main/java/FileSystem.java
@@ -7,7 +7,7 @@ import java.nio.file.Paths;
 import java.util.zip.DeflaterOutputStream;
 
 class FileSystem {
-    static void createPath(String path) {
+    void createPath(String path) {
         new File(path).mkdirs();
     }
 
